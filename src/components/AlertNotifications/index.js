abeab0e9da0b81d8f788357e0c5a0a12838c34bc
kottam rohith <kottamrohith123@gmail.com>
2023-06-09 11:46:56 +0000

    updated Notifications

--- src/components/AlertNotifications/index.js
+++ src/components/AlertNotifications/index.js
@@ -1,43 +1,55 @@
+import {AiFillCheckCircle} from 'react-icons/ai'
+import {RiErrorWarningFill} from 'react-icons/ri'
+import {MdWarning, MdInfo} from 'react-icons/md'
+
 import Notification from '../Notification'
 
 import './index.css'
-
-const notifications = [
-  {
-    id: 1,
-    Icon: 'AiFillCheckCircle',
-    Heading: 'Success',
-    Description: 'You can access all the files in the folder',
-  },
-  {
-    id: 2,
-    Icon: 'RiErrorWarningFill',
-    Heading: 'Error',
-    Description:
-      'Sorry, you are not authorized to have access to delete the file',
-  },
-  {
-    id: 3,
-    Icon: 'MdWarning',
-    Heading: 'Warning',
-    Description: 'Viewers of this file can see comments and suggestions',
-  },
-  {
-    id: 4,
-    Icon: 'MdInfo',
-    Heading: 'Info',
-    Description: 'Anyone on the internet can view these files',
-  },
-]
 
 const AlertNotifications = () => (
   <div className="AlertNotifications-container">
     <h1 className="AlertNotifications-heading"> Alert Notifications </h1>
 
     <ul className="unorderlist-container">
-      {notifications.map(each => (
-        <Notification key={each.id}>{each}</Notification>
-      ))}
+      <Notification>
+        <AiFillCheckCircle className="success icon" />
+        <div className="heading-container">
+          <h1 className="notification-heading succes-heading">Success</h1>
+          <p className="notification-para">
+            You can access all the files in the folder
+          </p>
+        </div>
+      </Notification>
+
+      <Notification>
+        <RiErrorWarningFill className="error icon" />
+        <div className="heading-container">
+          <h1 className="notification-heading error-heading">Error</h1>
+          <p className="notification-para">
+            Sorry, you are not authorized to have access to delete the file
+          </p>
+        </div>
+      </Notification>
+
+      <Notification>
+        <MdWarning className="warning icon" />
+        <div className="heading-container">
+          <h1 className="notification-heading warning-heading">Warning</h1>
+          <p className="notification-para">
+            Viewers of this file can see comments and suggestions
+          </p>
+        </div>
+      </Notification>
+
+      <Notification>
+        <MdInfo className="info icon" />
+        <div className="heading-container">
+          <h1 className="notification-heading info-heading">Info</h1>
+          <p className="notification-para">
+            Anyone on the internet can view these files
+          </p>
+        </div>
+      </Notification>
     </ul>
   </div>
 )
